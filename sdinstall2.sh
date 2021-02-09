85070c7bcc0dc8d299ecc28bd0bdc2d0834a3dc9
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-02-09 11:29:26 +0100

    Create sdinstall2.sh

---
+++ sdinstall2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# https://stackoverflow.com/questions/58302378/run-bash-script-as-root-in-startup-on-ubuntu-18-04
+
+cat > /etc/systemd/system/sd.sh <<EOF
+#!/bin/bash
+shutdown 120
+
+EOF
+
+cat > /etc/systemd/system/sd.service <<EOF
+
+[Unit]
+Description = sd.service
+
+[Service]
+WorkingDirectory= /etc/systemd/system/
+ExecStart= /etc/systemd/system/sd.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod +x /etc/systemd/system/sd.sh
+systemctl enable sd.service
+systemctl start sd.service
